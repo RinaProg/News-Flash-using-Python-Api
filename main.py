074ad90127a15603dd4e4153678606a756a7a789
Rina <rina@nath.com>
2023-03-04 20:57:54 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+from flask import Flask,render_template
+import requests
+from datetime import datetime
+
+
+app=Flask(__name__)
+
+url = ('https://newsapi.org/v2/everything?'
+       'q=Apple&'
+       'from=2023-02-11&'
+       'sortBy=popularity&'
+       'apiKey=c847eb5b70cc4740ad8642463ba94703')
+
+response = requests.get(url)
+news_data = response.json()
+now = datetime.now()
+time_news = now.time().strftime("%H:%M %p")
+
+now = datetime.now()
+date = now.strftime("%a,%d %b, %Y")
+
+@app.route('/')
+def home_news():
+  
+  return render_template('index.html',news_data = news_data,date=date,time_news=time_news)
+
+
+@app.route('/allnews')
+def all_news():
+
+ 
+  return render_template('all_news.html', news_data=news_data,date=date, time_news=time_news)  
+
+@app.route('/allnews2')
+def all_news2():
+
+ 
+  return render_template('all_news2.html', news_data=news_data,date=date, time_news=time_news)  
+
+
+if __name__=='__main__':
+    app.run(debug=True)
+
+
+
+
+
+
+
